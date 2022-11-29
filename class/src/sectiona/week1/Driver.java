828e87d9a1bb728b38276af8120bfeddf81dfade
John <34749334+JBukowy@users.noreply.github.com>
2022-11-29 08:54:29 -0600

    end of class 011 - w1d2

---
+++ class/src/sectiona/week1/Driver.java
@@ -0,0 +1,13 @@
+package sectiona.week1;
+
+public class Driver {
+
+    public static void main(String[] args) {
+
+        System.out.println('c');
+
+        System.out.println((char)('c'+1));
+
+    }
+
+}
---
+++ class/src/sectiona/week1/Rot.java
@@ -0,0 +1,21 @@
+package sectiona.week1;
+
+public class Rot {
+
+    public String encode(String phrase){
+
+        for(int i = 0; i<phrase.length(); i++){
+
+            char current;
+
+            current = phrase.charAt(i);
+
+            current += 13;
+
+        }
+
+        return "";
+
+    }
+
+}
