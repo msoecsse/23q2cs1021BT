f80a7d11d8b3339bd69717f7b220ce91b7d95697
Dr. Taylor <gitlab@taylor.bz>
2023-01-25 13:51:06 -0600

    Week 7 Lecture 1 for 061

--- class/src/sectionc/week7/Driver.java
+++ class/src/sectionc/week7/Driver.java
@@ -3,21 +3,41 @@ package sectionc.week7;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import static java.nio.file.StandardOpenOption.APPEND;
+
 public class Driver {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+        try (PrintWriter out = new PrintWriter(Files.newOutputStream(Path.of("thing.txt")))) {
+            out.print("""
+                            Demonstration Picture
+                            800 600
+                            #F0F0F0
+                            P: 40 40 #000000
+                            C: 40 40 #FF0000 30
+                            T: 60 60 #00FF00 100 50
+                            R: 160 160 #0000FF 50 150
+                            LT: 360 360 #00FF00 100 50 Tri
+                            LR: 460 460 #0000FF 150 100 Reck Tangle Angle Rangle""");
+        }
+    }
+
+    public static void main2(String[] args) {
         try {
             writeByte(Path.of("stuff.bin"), 72);
         } catch (IOException e) {
             System.err.println("Bad stuff happened, maybe");
+            e.printStackTrace();
         }
         System.out.println("I'm done");
     }
 
     public static void writeByte(Path path, int data) throws IOException {
-        try (OutputStream out = Files.newOutputStream(path)) {
+        try (OutputStream out = Files.newOutputStream(path, APPEND)) {
             out.write(data);
         }
     }
